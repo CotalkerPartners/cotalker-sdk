358683a52d2b46e5f0faf29d045d79b56c7df12e
JocelynTereucan <38298509+JocelynTereucan@users.noreply.github.com>
2025-03-28 09:20:51 -0300

    refactor(channel): improve implementation and file update

--- src/libs/models/COTSurveyClient.ts
+++ src/libs/models/COTSurveyClient.ts
@@ -1,5 +1,6 @@
 import {
 	COTSurvey,
+	COTSurveyChat,
 	SurveysQueryParams,
 	surveysQueryParams
 } from "@customTypes/COTTypes/COTSurvey";
@@ -9,25 +10,45 @@ import { queryValidator } from "@utils/QueryValidator";
 import { AxiosInstance } from "axios";
 
 export default class COTSurveyClient {
-	protected readonly _instance: AxiosInstance;
+	protected readonly axiosInstance: AxiosInstance;
 
 	private queryHandler;
 
 	public constructor(instance: AxiosInstance) {
-		this._instance = instance;
-		this.queryHandler = new QueryHandler("surveys", this._instance);
+		this.axiosInstance = instance;
+		this.queryHandler = new QueryHandler("surveys", this.axiosInstance);
 	}
 
 	public async getSurvey(surveyId: ObjectId): Promise<COTSurvey> {
 		return (
-			await this._instance.get<{ data: COTSurvey }>(
+			await this.axiosInstance.get<{ data: COTSurvey }>(
 				`/api/v2/surveys/${surveyId}?populate=true`
 			)
 		)?.data;
 	}
 
-	public async getSurveys(): Promise<COTSurvey[]> {
-		return this.queryHandler.getAllInQuery({});
+	public async getSurveys(query?: SurveysQueryParams): Promise<COTSurvey[]> {
+		if (query) {
+			queryValidator(surveysQueryParams, query);
+			return this.queryHandler.getAllInQuery(query);
+		}
+
+		let count = 1;
+		let page = 1;
+		const surveys: COTSurvey[] = [];
+
+		do {
+			const response = await this.axiosInstance.get<{
+				data: { count: number; surveys: COTSurvey[] };
+			}>(
+				`/api/v2/surveys?count=true&limit=100&page=${page}&isActive=true`
+			);
+			surveys.push(...response.data.surveys);
+			count = response.data.count;
+			page++;
+		} while (surveys.length < count);
+
+		return surveys;
 	}
 
 	public async getSurveyQuery(query: SurveysQueryParams): Promise<COTSurvey> {
@@ -50,5 +71,44 @@ export default class COTSurveyClient {
 		answerUuid: string | string[]
 	): Promise<COTSurvey[]> {
 		return this.queryHandler.getAllInQuery({ answer: answerUuid });
+	}
+
+	public async getSurveysByName(name: string): Promise<COTSurvey[]> {
+		const queryParams = name ? `?search=.*${name}` : "";
+		return (
+			(
+				await this.axiosInstance.get<{
+					data: { surveys: COTSurvey[] };
+				}>(`/api/v3/surveys${queryParams}`)
+			)?.data?.surveys || []
+		);
+	}
+
+	public async getSurveyChats(surveyID: ObjectId): Promise<COTSurveyChat[]> {
+		const surveyChats = (
+			await this.axiosInstance.get(
+				`/api/v2/surveychats?survey=${surveyID}`
+			)
+		).data?.surveyChats;
+		return surveyChats;
+	}
+
+	public async postSurveyChat(
+		surveyChat: Pick<COTSurveyChat, "contentArray" | "order" | "survey">
+	): Promise<COTSurveyChat> {
+		const body: Partial<COTSurveyChat> & { isNew: true } = {
+			survey: surveyChat.survey,
+			contentArray: surveyChat.contentArray,
+			order: surveyChat.order,
+			contentType: "application/vnd.cotalker.survey",
+			sender: "#system",
+			isNew: true
+		};
+		return (
+			await this.axiosInstance.post(
+				"/api/v2/surveychats?debug=true",
+				body
+			)
+		).data;
 	}
 }
