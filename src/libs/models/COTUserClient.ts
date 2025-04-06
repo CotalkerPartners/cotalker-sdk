307b5c307f7747762129d3a0e22f4c4cb3157321
JocelynTereucan <38298509+JocelynTereucan@users.noreply.github.com>
2025-04-05 23:28:16 -0400

    docs(user): Documentation is added with typedoc

--- src/libs/models/COTUserClient.ts
+++ src/libs/models/COTUserClient.ts
@@ -11,6 +11,9 @@ import { queryValidator } from "@utils/QueryValidator";
 import { AxiosInstance } from "axios";
 import * as querystring from "querystring";
 
+/**
+ * Manages user-related requests.
+ */
 export type AllowedRelation =
 	| "boss"
 	| "peers"
@@ -24,16 +27,31 @@ export default class COTUserClient {
 
 	private queryHandler;
 
+	/**
+	 * Constructs a new instance of the COTUserClient.
+	 * @param instance Axios instance used for HTTP requests.
+	 */
 	public constructor(instance: AxiosInstance) {
 		this.axiosInstance = instance;
 		this.queryHandler = new QueryHandler("users", this.axiosInstance);
 	}
+
+	/**
+	 * Gets users based on query parameters
+	 * @param query Query parameters for filtering users
+	 * @returns Promise that resolves to an array of users
+	 */
 
 	public async getUsersQuery(query: UsersQueryParams): Promise<COTUser[]> {
 		queryValidator(usersQueryParams, query);
 		return (await this.queryHandler.getQuery(query)).users;
 	}
 
+	/**
+	 * Gets all users that match the query parameters
+	 * @param query Query parameters to filter users
+	 * @returns Promise that resolves to an array of all matching users
+	 */
 	public async getAllUsersInQuery(
 		query: UsersQueryParams
 	): Promise<COTUser[]> {
@@ -41,6 +59,11 @@ export default class COTUserClient {
 		return this.queryHandler.getAllInQuery(query);
 	}
 
+	/**
+	 * Gets users by job ID
+	 * @param job Job ID
+	 * @returns Promise that resolves to an array of users with that job
+	 */
 	public async getUsersByJob(job: ObjectId): Promise<COTUser[]> {
 		let count = 1;
 		let page = 1;
@@ -58,10 +81,21 @@ export default class COTUserClient {
 		return users;
 	}
 
+	/**
+	 * Gets users by email address
+	 * @param email Email or array of emails to search for
+	 * @returns Promise that resolves to an array of users with those emails
+	 */
 	public async getUsersByEmail(email: string[] | string): Promise<COTUser[]> {
 		return this.getAllUsersInQuery({ email });
 	}
 
+	/**
+	 * Gets users by relationship
+	 * @param type Allowed relationship type
+	 * @param _id ID of the related object
+	 * @returns Promise that resolves to an array of users with that relationship
+	 */
 	public async getUsersByRelation(
 		type: AllowedRelation,
 		_id: ObjectId
@@ -75,11 +109,22 @@ export default class COTUserClient {
 		);
 	}
 
+	/**
+	 * Gets a user's activity
+	 * @param _id User ID
+	 * @returns Promise that resolves to the user's activity
+	 */
 	public async getUserActivity(_id: ObjectId): Promise<COTUserActivity> {
 		return (await this.axiosInstance.get(`/api/v2/user-activities/${_id}`))
 			.data;
 	}
 
+	/**
+	 * Applies JSON Patch operations to a user
+	 * @param userId ID of the user to modify
+	 * @param body Body with the JSON Patch operations
+	 * @returns Promise that resolves to the modified user
+	 */
 	public async jsonPatchUser<T extends COTUser>(
 		userId: ObjectId,
 		body: JSONPatchBody
@@ -92,6 +137,11 @@ export default class COTUserClient {
 		).data;
 	}
 
+	/**
+	 * Gets users by their IDs
+	 * @param ids Array of user IDs
+	 * @returns Promise that resolves to an array of found users
+	 */
 	public async getUsersById(ids: ObjectId[]): Promise<COTUser[]> {
 		const limit = 30;
 		const chunks = [];
@@ -112,6 +162,11 @@ export default class COTUserClient {
 		return results.flat();
 	}
 
+	/**
+	 * Search for users with a custom query
+	 * @param query Object with the search parameters
+	 * @returns Promise that resolves to an array of found users
+	 */
 	public async findUsers(query): Promise<COTUser[]> {
 		return this.axiosInstance.post(`/api/users/find?allFields=true`, {
 			"companies.companyId": "627400d234b48d5b6667db18",
@@ -120,6 +175,11 @@ export default class COTUserClient {
 		});
 	}
 
+	/**
+	 * Gets a user's superior users (bosses)
+	 * @param userId User ID
+	 * @returns Promise that resolves to an array of superior users
+	 */
 	public async getBossUsers(userId: ObjectId): Promise<COTUser[]> {
 		return (
 			(
